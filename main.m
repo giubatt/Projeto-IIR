470e61df2ac19439d9ec6496772a0aac964e1672
Giuseppe Battistella <bartgb00@gmail.com>
2016-06-25 15:29:56 -0300

    start
    added testeSimetria.m
    started main.m

--- main.m
+++ main.m
@@ -1,3 +1,16 @@
-asd
+% Projeto Filtro IIR
+% Programa principal
 
-%oi main
+% Autores: Lucas Fernandes e Giuseppe Battistella
+% Data: 25/06/2016
+
+clc;clear;close;
+
+%% Especificacoes %
+fp = [0.6 1.6]*10^3;	% Limites da banda passante
+fs = [0.25 2.4]*10^3;	% Limites da banda de rejeicao
+ft = 8*10^3;            % Frequencia de amostragem
+Ap = 1;                 % Ripple na banda passante
+As = 35;                % Atenuacao minima da banda de rejeicao
+% -------- %
+
---
+++ testeSimetria.m
@@ -0,0 +1,22 @@
+% Projeto Filtro IIR
+% Teste de simetria
+% Calcula ws e wp que satisfaçam a simetria, sem modificar wp
+
+% Autores: Lucas Fernandes e Giuseppe Battistella
+% Data: 25/06/2016
+
+% in:
+% ws = frequencias de banda de rejeicao
+% wp = frequencias de banda passante
+
+% out:
+% ws = freq de rejeicao simetricas em relacao a wp
+% wp = freq de banda passante
+
+function [wp, ws] = testeSimetria(wp, ws)
+    if (ws(1)*ws(2) < wp(1)*wp(2))
+        ws(1)=wp(1)*wp(2)/ws(2);
+    else
+        ws(2)=wp(1)*wp(2)/ws(1);
+    end
+end
